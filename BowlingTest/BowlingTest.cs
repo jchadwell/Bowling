256b095331c60215cf1d2bc7259f10b70003c2f0
Jason Chadwell <jchadwell@gmail.com>
2014-04-29 15:59:41 -0400

    first commit

---
+++ BowlingTest/BowlingTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingTest
+{
+    [TestClass]
+    public class BowlingTest
+    {
+        private Game g;
+
+        private void rollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                g.roll(pins);
+            }
+        }
+
+        private void rollSpare()
+        {
+            g.roll(5);
+            g.roll(5);
+        }
+
+        private void rollStrike()
+        {
+            g.roll(10);
+        }
+
+        [TestInitialize]
+        public void setUp()
+        {
+            g = new Game();
+        }
+
+        [TestMethod]
+        public void gutterGame()
+        {
+            rollMany(20, 0);
+            Assert.AreEqual(0, g.Score());
+        }
+
+        [TestMethod]
+        public void allOnes()
+        {
+            rollMany(20, 1);
+            Assert.AreEqual(20, g.Score());
+        }
+
+        [TestMethod]
+        public void oneSpare()
+        {
+            rollSpare();
+            g.roll(3);
+            rollMany(17, 0);
+            Assert.AreEqual(16, g.Score());
+        }
+
+        [TestMethod]
+        public void oneStrike()
+        {
+            rollStrike();
+            g.roll(3);
+            g.roll(4);
+            rollMany(16, 0);
+            Assert.AreEqual(24, g.Score());
+        }
+
+        [TestMethod]
+        public void perfectGame()
+        {
+            rollMany(12, 10);
+            Assert.AreEqual(300, g.Score());
+        }
+    }
+}
---
+++ BowlingTest/Game.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BowlingTest
+{
+    public class Game
+    {
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
+
+        public void roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int firstInFrame = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (isStrike(firstInFrame))
+                {
+                    score += nextTwoBallsForStrike(firstInFrame);
+                    firstInFrame++;
+                }
+                else if (isSpare(firstInFrame))
+                {
+                    score += nextBallForSpare(firstInFrame);
+                    firstInFrame += 2;
+                }
+                else
+                {
+                    score += twoBallsInFrame(firstInFrame);
+                    firstInFrame += 2;
+                }
+            }
+            return score;
+        }
+
+        private int twoBallsInFrame(int firstInFrame)
+        {
+            return rolls[firstInFrame] + rolls[firstInFrame + 1];
+        }
+
+        private int nextBallForSpare(int firstInFrame)
+        {
+            return 10 + rolls[firstInFrame + 2];
+        }
+
+        private int nextTwoBallsForStrike(int firstInFrame)
+        {
+            return 10 + rolls[firstInFrame + 1] + rolls[firstInFrame + 2];
+        }
+
+        private bool isStrike(int firstInFrame)
+        {
+            return rolls[firstInFrame] == 10;
+        }
+
+        private bool isSpare(int firstInFrame)
+        {
+            return rolls[firstInFrame] + rolls[firstInFrame + 1] == 10;
+        }
+    }
+}
